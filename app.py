f2119856a4fc090bac944393c21b07e1227b4060
mabiesen <mabiesen@outlook.com>
2018-06-02 15:36:58 -0500

    successful cors

--- app.py
+++ app.py
@@ -1,36 +1,64 @@
 #!flask/bin/python
-from flask import Flask, request, jsonify, make_response
-from flask_httpauth import HTTPBasicAuth
+from flask import Flask, abort, request, jsonify, make_response, current_app
 from myemail import myemail
-
-
-auth = HTTPBasicAuth()
+from flask_cors import CORS, cross_origin
+from functools import update_wrapper
+from datetime import timedelta  
 
 app = Flask(__name__)
 
+def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_to_all=True, automatic_options=True):  
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
 
-@auth.get_password
-def get_password(username):
-    if username == 'miguel':
-        return 'python'
-    print("here")
-    return None
+    def get_methods():
+        if methods is not None:
+            return methods
 
-@auth.error_handler
-def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+	    h['Content-Type'] = 'application/json'
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 
-@app.route('/email', methods=['POST'])
-@auth.login_required
+
+@app.route("/email", methods=['POST','GET','OPTIONS'])
+@crossdomain(origin=['*'],headers=['Content-Type'],methods=["GET","POST","OPTIONS"])
 def create_task():
-    if not request.json or not 'recipient' in request.json:
-        abort(400)
-   
+    print(request.json['recipient'])
+    print("here3")
     emailc = myemail()
     emailc.send_email(request.json['recipient'])
-    
-    return jsonify({'task': 'succeeded!'}), 201
+    myreturn = jsonify({'task': 'succeeded!'}), 201 
+    return myreturn
+
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True, port=3000)
+    app.run(host='0.0.0.0',debug=True, threaded=True, port=3000)
